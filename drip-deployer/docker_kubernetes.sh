6ce0e1cb9d4ef11728d44d309ab8a0801bd380fd
Oceans <y.hu@uva.nl>
2017-05-09 16:18:49 +0200

    kubernetes

--- drip-deployer/docker_kubernetes.sh
+++ drip-deployer/docker_kubernetes.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+sudo apt-get update && apt-get install -y apt-transport-https
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker-engine
 sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
