5edac92ea025292b02cbf1a133c277756e419665
skoulouzis <S.Koulouzis@uva.nl>
2017-10-12 13:52:13 +0200

    add methods to make code more readable

--- drip_parser/src/transformer/docker_compose_transformer.py
+++ drip_parser/src/transformer/docker_compose_transformer.py
@@ -27,54 +27,78 @@ class DockerComposeTransformer:
         return self.yaml_dict_tpl['node_types']
     
     def get_docker_types(self):
-        docker_types = []
+        docker_types = set([])
         node_types = self.get_node_types()
         for node_type_key in node_types:
             if node_types[node_type_key] and 'derived_from' in node_types[node_type_key].keys():
                 if node_types[node_type_key]['derived_from'] == self.DOCKER_TYPE:
-                    docker_types.append(node_type_key)
+                    docker_types.add(node_type_key)
         return  docker_types
     
     def get_node_templates(self):
         return self.yaml_dict_tpl['topology_template']['node_templates']
     
+    def get_artifacts(self,node):
+        if 'artifacts' in node:
+            return node['artifacts']
+        
+    def get_properties(self,node):
+        if 'properties' in node:
+            return node['properties']
+    
+    def get_enviroment_vars(self,properties):
+        environment = []
+        for prop in properties:
+            if properties[prop] and not isinstance(properties[prop],dict):
+                environment.append(prop+"="+str(properties[prop]))
+        return environment
+    
+    def get_port_map(self,properties):
+        if 'ports_mapping' in properties:
+            ports_mappings = properties['ports_mapping']
+            
+            port_maps = []
+            for port_map_key in ports_mappings:            
+                host_port =  ports_mappings[port_map_key]['host_port']
+                if not isinstance(host_port, (int, long, float, complex)):
+                    host_port_var =  host_port.replace('${','').replace('}','')
+                    host_port = properties[host_port_var]
+
+                container_port =  ports_mappings[port_map_key]['container_port']
+                if not isinstance(container_port, (int, long, float, complex)):
+                    container_port_var =  container_port.replace('${','').replace('}','')
+                    container_port = properties[container_port_var]
+                port_maps.append(str(host_port)+':'+str(container_port))
+        return port_maps
+    
+    
     def analyze_yaml(self):
         docker_types  = self.get_docker_types()
         node_templates =  self.get_node_templates() 
+        services = []
         
-        services = {}
         for node_template_key in node_templates:
             for docker_type in docker_types:
                 if docker_type in node_templates[node_template_key]['type']:
-                    if 'artifacts' in node_templates[node_template_key]:
-                        artifacts = node_templates[node_template_key]['artifacts']
+                    service = {}
+                    service['id'] = node_template_key
+                    artifacts = self.get_artifacts(node_templates[node_template_key])
+                    if artifacts:
                         key =  next(iter(artifacts))
                         docker_file =  artifacts[key]['file']
-                        services['name'] = docker_file
-                        services['image'] = docker_file
-                        services['id'] = node_template_key
-                if 'properties' in node_templates[node_template_key]:
-                    properties =  node_templates[node_template_key]['properties']
-                    environment = []
-                    for prop in properties:
-                        if not isinstance(properties[prop],dict):
-                            environment.append(prop+"="+str(properties[prop]))
-                    services['environment'] = environment
-                    if 'ports_mapping' in properties:
-                        ports_mappings = properties['ports_mapping']
-                        key =  next(iter(ports_mappings))
-                        host_port =  ports_mappings[key]['host_port']
-                        if not isinstance(host_port, (int, long, float, complex)):
-                            host_port_var =  host_port.replace('${','').replace('}','')
-                            host_port = properties[host_port_var]
-                        container_port =  ports_mappings[key]['container_port']
-                        if not isinstance(container_port, (int, long, float, complex)):
-                            container_port_var =  container_port.replace('${','').replace('}','')
-                            container_port = properties[container_port_var]
-                        ports = []
-                        ports.append(str(host_port)+':'+str(container_port))
-                        services['ports'] = ports
-        print services
+                        service['name'] = docker_file
+                        service['image'] = docker_file
+                    
+                    properties = self.get_properties(node_templates[node_template_key])
+                    environment = self.get_enviroment_vars(properties)
+                    service['environment'] = environment
+                    
+                    port_maps = self.get_port_map(properties)
+                    service['ports'] = port_maps
+                    services.append(service)
+#                    print service
+                    break
+        return services
 
     def analize_tosca():
         dockers = []
