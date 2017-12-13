cdd38b40637e5319331591ea404a73acce3ac9b9
skoulouzis <S.Koulouzis@uva.nl>
2017-12-13 19:39:09 +0100

    add wave plugin to solve broadcast issue

--- drip-deployer/docker_engine.sh
+++ drip-deployer/docker_engine.sh
@@ -10,34 +10,7 @@ sudo apt-get -y install docker-ce
 #sudo echo "{ \"insecure-registries\":[\"129.7.98.3:5000\"] }" > /etc/docker/daemon.json
 #sudo service docker restart
 
-# sudo apt-get -y install firewalld
-# sudo systemctl start firewalld
-# sudo systemctl enable firewalld
-
-# sudo firewall-cmd --add-port=22/tcp --permanent
-# sudo firewall-cmd --add-port=2376/tcp --permanent
-# sudo firewall-cmd --add-port=2377/tcp --permanent
-# sudo firewall-cmd --add-port=7946/tcp --permanent
-# sudo firewall-cmd --add-port=7946/udp --permanent
-# sudo firewall-cmd --add-port=4789/udp --permanent
-# sudo firewall-cmd --reload
-# sudo systemctl restart docker
-
-
-sudo echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-sudo echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-
-sudo apt-get -y install iptables-persistent
-sudo netfilter-persistent flush
-
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 2376 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 2377 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 7946 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 7946 -j ACCEPT
-sudo iptables -A INPUT -p udp --dport 4789 -j ACCEPT
-
-sudo netfilter-persistent save
-sudo systemctl restart docker
-
-    
+sudo docker plugin install --grant-all-permissions weaveworks/net-plugin:latest_release
+sudo docker plugin disable weaveworks/net-plugin:latest_release
+sudo docker plugin set weaveworks/net-plugin:latest_release WEAVE_MULTICAST=1
+sudo docker plugin enable weaveworks/net-plugin:latest_release
