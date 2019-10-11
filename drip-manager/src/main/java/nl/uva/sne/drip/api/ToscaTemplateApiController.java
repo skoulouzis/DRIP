aab6260fe2ac16ce38fc1ec8ea92918e8f16d9bd
skoulouzis <S.Koulouzis@uva.nl>
2019-10-11 17:10:02 +0200

    return void

--- drip-manager/src/main/java/nl/uva/sne/drip/api/ToscaTemplateApiController.java
+++ drip-manager/src/main/java/nl/uva/sne/drip/api/ToscaTemplateApiController.java
@@ -42,13 +42,8 @@ public class ToscaTemplateApiController implements ToscaTemplateApi {
     public ResponseEntity<String> deleteToscaTemplateByID(@ApiParam(value = "ID of topolog template to return", required = true) @PathVariable("id") String id) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("*/*")) {
-            try {
-                toscaTemplateService.deleteByID(id);
-                return new ResponseEntity<>(objectMapper.readValue("\"\"", String.class), HttpStatus.OK);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            toscaTemplateService.deleteByID(id);
+            return new ResponseEntity<>("", HttpStatus.OK);
         }
 
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
