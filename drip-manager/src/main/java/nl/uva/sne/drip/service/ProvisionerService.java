f5f5c03c023b49409855d326b4ac0f38ab2cf306
skoulouzis <S.Koulouzis@uva.nl>
2019-10-25 17:07:00 +0200

    Added tests for utils updated model

--- drip-manager/src/main/java/nl/uva/sne/drip/service/ProvisionerService.java
+++ drip-manager/src/main/java/nl/uva/sne/drip/service/ProvisionerService.java
@@ -6,6 +6,8 @@
 package nl.uva.sne.drip.service;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,24 +24,31 @@ import org.springframework.stereotype.Service;
  * @author S. Koulouzis
  */
 @Service
-public class ProvisionerService  {
+public class ProvisionerService {
 
     @Value("${message.broker.queue.provisioner}")
     private String queueName;
-    
+
     @Value("${tosca.types.interface}")
-    private String toscaTypesInterface;    
+    private String toscaTypesInterface;
 
     @Autowired
     private ToscaTemplateService toscaTemplateService;
 
     public String provision(String id) throws IOException {
-        
+
         String ymlToscaTemplate = toscaTemplateService.findByID(id);
         ToscaTemplate toscaTemplate = toscaTemplateService.getYaml2ToscaTemplate(ymlToscaTemplate);
-        
+        List<Map.Entry> vmTopologies = getVmTopologies(toscaTemplate);
+        for(Map.Entry topology: vmTopologies){
+            topology.
+        }
         
         
         return null;
+    }
+
+    public List<Map.Entry> getVmTopologies(ToscaTemplate toscaTemplate) {
+        return TOSCAUtils.getNodesByType(toscaTemplate, "tosca.nodes.ARTICONF.VM.topology");
     }
 }
