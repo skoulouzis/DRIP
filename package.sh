565ab013cc1d26ecbc7d4969d4c26f7d4a027faa
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 20:54:47 +0100

    added new dependencies for the 'package javax.xml.bind.annotation does not exist' error

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ echo "----------Building drip-provisioner Docker--------------"
 cd drip-provisioner && mvn -Dmaven.test.skip=true dockerfile:build
 cd ../
 echo "----------Building sure_tosca-flask-server Docker--------------"
-cd sure_tosca-flask-server && docker build -t deth53/sure-tosca:3.0.0 .
+cd sure_tosca-flask-server && docker build -t sure-tosca:3.0.0 .
 echo "----------Building drip-planner Docker--------------"
 cd ../
-cd drip-planner && docker build -t deth53/drip-planner:3.0.0 .
+cd drip-planner && docker build -t drip-planner:3.0.0 .
