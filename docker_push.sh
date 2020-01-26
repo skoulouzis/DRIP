bd083b6aabc1f84fb2366bfaeab75a625f1d19a3
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 21:06:43 +0100

    add correct prefix

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker push deth53/drip-manager:3.0.0
-docker push deth53/drip-provisioner:3.0.0
-docker push deth53/sure-tosca:3.0.0
-docker push deth53/drip-planner:3.0.0
+docker push alogo53/drip-manager:3.0.0
+docker push alogo53/drip-provisioner:3.0.0
+docker push alogo53/sure-tosca:3.0.0
+docker push alogo53/drip-planner:3.0.0
