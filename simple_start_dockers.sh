aae715aff28819b11c7bba3b60827a1434a5e8d4
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 21:32:39 +0100

    add simple start script

---
+++ simple_start_dockers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo docker stop $(sudo docker ps -q)
+
+docker start mongo-inst
+sleep 1
+docker start some-rabbit
+sleep 1
+docker run -d alogo53/sure-tosca:3.0.0
+sleep 5
+docker run -it -e MONOG_HOST=172.17.0.2 -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=172.17.0.4/8081/tosca-sure/1.0.0 -p 8080:8080 alogo53/drip-manager:3.0.0
