90456117b97468720be61312107f113022e237ad
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 20:16:57 +0100

    fixed docker build

--- package.sh
+++ package.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+echo "----------Building drip-manager Docker--------------"
 cd drip-manager && mvn -Dmaven.test.skip=true dockerfile:build
 cd ../
-cd drip-planner && mvn -Dmaven.test.skip=true dockerfile:build
+echo "----------Building drip-provisioner Docker--------------"
+cd drip-provisioner && mvn -Dmaven.test.skip=true dockerfile:build
 cd ../
-cd sure_tosca-flask-server && docker build -t sure-tosca:3.0.0 .
-
+echo "----------Building sure_tosca-flask-server Docker--------------"
+cd sure_tosca-flask-server && docker build -t deth53/sure-tosca:3.0.0 .
+echo "----------Building drip-planner Docker--------------"
 cd ../
-cd drip-planner && docker build -t drip-planner:3.0.0 .
+cd drip-planner && docker build -t deth53/drip-planner:3.0.0 .
