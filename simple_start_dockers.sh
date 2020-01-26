791e547b2ce6d788d99e676bd418e77daccb7008
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 22:06:03 +0100

    escape chars in url

--- simple_start_dockers.sh
+++ simple_start_dockers.sh
@@ -8,4 +8,4 @@ docker start some-rabbit
 sleep 1
 docker run -d alogo53/sure-tosca:3.0.0
 sleep 5
-docker run -it -e MONOG_HOST=172.17.0.2 -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=172.17.0.4/8081/tosca-sure/1.0.0 -p 8080:8080 alogo53/drip-manager:3.0.0
+docker run -it -e MONOG_HOST=172.17.0.2 -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-manager:3.0.0
