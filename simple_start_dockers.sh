54572cebedd09294b49ecfb04e040b5e8a2dd263
skoulouzis <S.Koulouzis@uva.nl>
2020-01-26 22:21:58 +0100

    fix wrong var name

--- simple_start_dockers.sh
+++ simple_start_dockers.sh
@@ -3,15 +3,15 @@
 sudo docker stop $(sudo docker ps -q)
 
 docker start mongo-inst
-sleep 1
+sleep 2
 docker start some-rabbit
-sleep 1
-docker run -d alogo53/sure-tosca:3.0.0
-sleep 5
-docker run -it -e MONOG_HOST=172.17.0.2 -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-manager:3.0.0
+sleep 2
+docker run -d sure-tosca:3.0.0
+sleep 6
+docker run -it -e MONOG_HOST=172.17.0.2 -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH='http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0\/' -p 8080:8080 drip-manager:3.0.0
 
 
-docker run -it -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-planner:3.0.0
-
-
-docker run -it -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-provisioner:3.0.0
+# docker run -it -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-planner:3.0.0
+# 
+# 
+# docker run -it -e RABBITMQ_HOST=172.17.0.3 -e SURE_TOSCA_BASE_PATH=http\:\/\/172.17.0.4\/8081\/tosca-sure\/1.0.0/ -p 8080:8080 alogo53/drip-provisioner:3.0.0
