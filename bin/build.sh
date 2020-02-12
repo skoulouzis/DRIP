0db14a92048152179c66ac23df256bc4f079d942
skoulouzis <S.Koulouzis@uva.nl>
2020-02-12 15:43:23 +0100

    created test docker

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+cd ../
+
 mvn -Dmaven.test.skip=true install
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 
 cd planner && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 cd ../
 cd sure_tosca-flask-server && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt && venv/bin/pip3 install -r test-requirements.txt
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 cd ../
 cd deployer && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
--- bin/test.sh
+++ bin/test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 mvn test
-
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 cd drip-planner && venv/bin/python3 -m unittest discover
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 cd ../
 cd sure_tosca-flask-server && venv/bin/python3 -m unittest discover
+status=$?
+[ $status -eq 0 ] && echo "build successful" || exit -1
 
 
