0e6a1913cfd75eb61de8ac2d14a6b4125aa3cc26
skoulouzis <S.Koulouzis@uva.nl>
2020-03-10 18:07:02 +0100

    fixed testing

--- bin/test.sh
+++ bin/test.sh
@@ -4,9 +4,11 @@ cd ../
 mvn test
 status=$?
 [ $status -eq 0 ] && echo "build successful" || exit -1
-cd drip-planner && venv/bin/python3 -m unittest discover
+
+cd planner && venv/bin/python3 -m unittest discover
 status=$?
 [ $status -eq 0 ] && echo "build successful" || exit -1
+
 cd ../
 cd sure_tosca-flask-server && venv/bin/python3 -m unittest discover
 status=$?
