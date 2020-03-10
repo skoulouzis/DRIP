1da0bc2592ed65bdeac2800743a56b63b3168672
skoulouzis <S.Koulouzis@uva.nl>
2020-03-10 18:09:00 +0100

    added dbg msg in test script

--- bin/test.sh
+++ bin/test.sh
@@ -3,15 +3,15 @@
 cd ../
 mvn test
 status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
+[ $status -eq 0 ] && echo "-------Java tests successful------" || exit -1
 
 cd planner && venv/bin/python3 -m unittest discover
 status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
+[ $status -eq 0 ] && echo "-------Planner tests successful------" || exit -1
 
 cd ../
 cd sure_tosca-flask-server && venv/bin/python3 -m unittest discover
 status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
+[ $status -eq 0 ] && echo "-------sure_tosca-flask-server tests successful------"" || exit -1
 
 
