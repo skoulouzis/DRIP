74ba1d6172718c49a1b9641c2507b521f8df9687
skoulouzis <S.Koulouzis@uva.nl>
2020-04-23 14:53:10 +0200

    change docker repo , update build docker_push and dockerize

--- bin/build.sh
+++ bin/build.sh
@@ -6,14 +6,25 @@ mvn -Dmaven.test.skip=true install
 status=$?
 [ $status -eq 0 ] && echo "build successful" || exit -1
 
-cd planner && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt
-status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
-cd ../
-cd sure_tosca-flask-server && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt && venv/bin/pip3 install -r test-requirements.txt
-status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
-cd ../
-cd deployer && rm -rf venv && python3 -m venv venv && venv/bin/pip3 install -r requirements.txt
-status=$?
-[ $status -eq 0 ] && echo "build successful" || exit -1
+
+
+python_projects=(sure_tosca-flask-server sure_tosca-client_python_stubs semaphore-python-client-generated planner deployer)
+
+for python_project in ${python_projects[*]}
+do
+    echo "----------------- Building:  $python_project---------------" 
+    cd $python_project 
+    rm -rf venv 
+    python3 -m venv venv
+    venv/bin/pip3 install -r requirements.txt
+    if [ "$python_project" == "deployer" ]; then
+        cd ../semaphore-python-client-generated
+        ../$python_project/venv/bin/python setup.py install
+        cd ../sure_tosca-client_python_stubs/
+        ../$python_project/venv/bin/python setup.py install
+    fi
+    status=$?
+    [ $status -eq 0 ] && echo "----------------Build: $python_project successful ------"  || exit -1
+    cd ../
+done
+
--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker push alogo53/manager:3.0.0
-docker push alogo53/provisioner:3.0.0
-docker push alogo53/sure-tosca:3.0.0
-docker push alogo53/planner:3.0.0
-docker push alogo53/deployer:3.0.0
+docker push qcdia/manager:3.0.0
+docker push qcdia/provisioner:3.0.0
+docker push qcdia/sure-tosca:3.0.0
+docker push qcdia/planner:3.0.0
+docker push qcdia/deployer:3.0.0
