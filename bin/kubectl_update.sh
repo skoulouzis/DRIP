c2921364b4fb64f9a7fe903c5de427270aed4fb9
skoulouzis <S.Koulouzis@uva.nl>
2020-05-11 15:57:06 +0200

    updated travis. added namespace to update. added nginx reverse proxy

--- bin/kubectl_update.sh
+++ bin/kubectl_update.sh
@@ -5,8 +5,8 @@ deployments=(manager sure-tosca planner provisioner deployer)
 for deployment in ${deployments[*]}
 do
     echo "----------------- updating image for $deployment---------------" 
-    kubectl set image deployment $deployment $deployment=qcdis/$deployment:3.0.0
-    kubectl rollout history deployment $deployment
+    kubectl set image deployment $deployment $deployment=qcdis/$deployment:3.0.0 -n conf
+    kubectl rollout history deployment $deployment -n conf
 done
 
 
