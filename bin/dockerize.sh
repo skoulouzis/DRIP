8b77f819827895662710868d8618f20f76cccc9f
skoulouzis <S.Koulouzis@uva.nl>
2020-06-18 13:57:30 +0200

    removed version

--- bin/dockerize.sh
+++ bin/dockerize.sh
@@ -10,21 +10,21 @@ cd provisioner && mvn -Dmaven.test.skip=true dockerfile:build
 [ $status -eq 0 ] && echo "build successful" || exit -1
 cd ../
 echo "----------Building sure_tosca-flask-server Docker--------------"
-cd sure_tosca-flask-server && docker build -t sure-tosca:3.0.0 .
+cd sure_tosca-flask-server && docker build -t sure-tosca .
 [ $status -eq 0 ] && echo "build successful" || exit -1
 echo "----------Building planner Docker--------------"
 cd ../
-cd planner && docker build -t planner:3.0.0 .
+cd planner && docker build -t planner .
 [ $status -eq 0 ] && echo "build successful" || exit -1
 echo "----------Building deployer Docker--------------"
 cd ../
-cd deployer && docker build -t deployer:3.0.0 .
+cd deployer && docker build -t deployer .
 [ $status -eq 0 ] && echo "build successful" || exit -1
 
 
 
-docker tag sure-tosca:3.0.0 qcdis/sure-tosca:3.0.0
-docker tag planner:3.0.0 qcdis/planner:3.0.0
-docker tag deployer:3.0.0 qcdis/deployer:3.0.0
-docker tag manager:3.0.0 qcdis/manager:3.0.0
+docker tag sure-tosca qcdis/sure-tosca
+docker tag planner qcdis/planner
+docker tag deployer qcdis/deployer
+docker tag manager:3.0.0 qcdis/manager
 docker tag provisioner:3.0.0 qcdis/provisioner
